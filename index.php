20cf28ffd0fa54cbaea693cdef9f37e7beab2906
yancell <35684687+yancell@users.noreply.github.com>
2022-09-06 15:52:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -242,9 +242,9 @@
                     <div class="col-lg-4 mb-5 mb-lg-0">
                         <h4 class="text-uppercase mb-4">Location</h4>
                         <p class="lead mb-0">
-                            2215 John Daniel Drive
+                            Karawang
                             <br />
-                            Clark, MO 65243
+                            Indonesia
                         </p>
                     </div>
                     <!-- Footer Social Icons-->
